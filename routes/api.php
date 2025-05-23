d20006dfe5afdef989a1dd5d9ea83baf3468974c
moiz-saviours <moiz@saviours.co>
2025-05-23 21:00:17 +0500

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -43,18 +43,14 @@ Route::post('stripe-process-payment', [ApiStripePaymentController::class, 'proce
 Route::post('paypal-create-order', [ApiPaypalPaymentController::class, 'createOrder'])->name('api.paypal.create-order');
 Route::post('paypal-capture-order', [ApiPaypalPaymentController::class, 'captureOrder'])->name('api.paypal.capture-order');
 Route::post('paypal-cancel-order', [ApiPaypalPaymentController::class, 'cancelOrder'])->name('api.paypal.cancel-order');
-
-Route::post('upload-payment-proof',[ApiPaymentAttachmentController::class,'upload_attachment'])->name('api.upload-payment-proof');
-
-
+Route::post('upload-payment-proof', [ApiPaymentAttachmentController::class, 'upload_attachment'])->name('api.upload-payment-proof');
 //});
 Route::get('fetch-invoice/{invoice?}', [ApiInvoiceController::class, 'fetch_invoice'])->missing(function (Request $request) {
     return response()->json(['error' => 'Invalid url.'], 404);
 });
-Route::post('/api/check-user', function(Request $request) {
+Route::post('/api/check-user', function (Request $request) {
     $email = $request->input('email');
     $table = $request->input('table');
-
     if (!$email || !$table) {
         return response()->json(['error' => 'Missing email or table'], 400);
     }
@@ -63,13 +59,11 @@ Route::post('/api/check-user', function(Request $request) {
         return response()->json(['error' => 'Invalid table'], 400);
     }
     $exists = DB::table($table)->where('email', $email)->exists();
-
     return response()->json(['exists' => $exists]);
 });
-Route::post('/check-channels', function(Request $request) {
+Route::post('/check-channels', function (Request $request) {
 
     $tableToCheck = ($request->has('type') && $request->get('type') === 999) ? 'admins' : 'users';
-
     $channels = [
         'payusinginvoice' => 'Channel 1',
         'paymentbyinvoice' => 'Channel 2',
@@ -77,32 +71,32 @@ Route::post('/check-channels', function(Request $request) {
         'paythroughinvoice' => 'Channel 4',
         'payviainvoice' => 'Channel 5',
     ];
-
     $validChannels = [];
-
-    foreach ($channels as $domain  => $channelName) {
+    foreach ($channels as $domain => $channelName) {
         try {
             $response = Http::timeout(3)->post("https://{$domain}.com/api/check-user", [
                 'email' => $request->email,
                 'table' => $tableToCheck
             ]);
-
             if ($response->ok() && $response->json('exists')) {
                 $validChannels[] = [
                     'domain' => $domain,
                     'name' => $channelName,
                 ];
             }
+            $response_req = [
+                'domain' => $domain,
+                'channels' => $validChannels,
+                'email' => $request->email,
+                'table' => $tableToCheck,
+                'name' => $channelName,
+            ];
         } catch (\Exception $e) {
             dd($e->getMessage());
         }
     }
-
-    return response()->json($validChannels);
+    return response()->json(['validChannels' => $validChannels, 'response_req' => $response_req]);
 })->name('check.channels');
-
-
-
 Route::fallback(function () {
     return response()->json(['error' => 'Controller or function not found'], 404);
 });
