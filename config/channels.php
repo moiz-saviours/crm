f8d3791127cc727cb23351cf86e9471d98d8285d
moiz-saviours <moiz@saviours.co>
2025-05-28 01:35:16 +0500

    channels updated
    channels updated

--- config/channels.php
+++ config/channels.php
@@ -1,10 +1,10 @@
 <?php
 return [
-    'payusinginvoice' => 'Channel 1',
-    'paymentbyinvoice' => 'Channel 2',
-    'paymentviainvoice' => 'Channel 3',
-    'paythroughinvoice' => 'Channel 4',
-    'payviainvoice' => 'Channel 5',
+    'payusinginvoice' => 'Pay using invoice',
+    'paymentbyinvoice' => 'Payment by invoice',
+    'paymentviainvoice' => 'Payment via invoice',
+    'paythroughinvoice' => 'Pay through invoice',
+    'payviainvoice' => 'Pay via invoice',
     'localhost:8000' => 'Local Channel 8000',
     'localhost:8001' => 'Local Channel 8001',
     'localhost:8002' => 'Local Channel 8002',
