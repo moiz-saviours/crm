67b77357d92ce335aba70ca8edc28dbc1549dc36
moiz-saviours <moiz@saviours.co>
2025-05-28 00:56:17 +0500

    feat(config): add channels configuration file
    feat(config): add channels configuration file
    - Production and local channel mappings
    - Simple key-value structure
    - Ready for channel detection

---
+++ config/channels.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'payusinginvoice' => 'Channel 1',
+    'paymentbyinvoice' => 'Channel 2',
+    'paymentviainvoice' => 'Channel 3',
+    'paythroughinvoice' => 'Channel 4',
+    'payviainvoice' => 'Channel 5',
+    'localhost:8000' => 'Local Channel 8000',
+    'localhost:8001' => 'Local Channel 8001',
+    'localhost:8002' => 'Local Channel 8002',
+];
