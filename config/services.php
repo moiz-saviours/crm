dad3c2838b60b1c60d9ebda222b4625d6e21895b
moiz-saviours <moiz@saviours.co>
2025-06-18 02:33:53 +0500

    feat(config): add Twilio SMS service configuration
    feat(config): add Twilio SMS service configuration
    
    - Added Twilio SID, token and from number
    - Implemented environment variable support
    - Included default test credentials
    - Followed config file standards

--- config/services.php
+++ config/services.php
@@ -1,7 +1,5 @@
 <?php
-
 return [
-
     /*
     |--------------------------------------------------------------------------
     | Third Party Services
@@ -13,26 +11,26 @@ return [
     | a conventional file to locate the various service credentials.
     |
     */
-
     'postmark' => [
         'token' => env('POSTMARK_TOKEN'),
     ],
-
     'ses' => [
         'key' => env('AWS_ACCESS_KEY_ID'),
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
-
     'resend' => [
         'key' => env('RESEND_KEY'),
     ],
-
     'slack' => [
         'notifications' => [
             'bot_user_oauth_token' => env('SLACK_BOT_USER_OAUTH_TOKEN'),
             'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
         ],
     ],
-
+    'twilio' => [
+        'sid' => env('TWILLO_SID'),
+        'token' => env('TWILIO_TOKEN'),
+        'from' => env('TWILIO_FROM'),
+    ],
 ];
