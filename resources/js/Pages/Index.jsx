0e9fbb368925bdeaf6ce6e2c5dd8c43294c8ddac
moiz-saviours <moiz@saviours.co>
2025-08-07 04:11:52 +0500

    Hussain khan ( update ): Add Index page component
    Introduces the Index.jsx component, which renders MainLayout, Board, and BoardList components. This serves as the main entry point for the page.

---
+++ resources/js/Pages/Index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import MainLayout from "../Layouts/MainLayout.jsx";
+import Board from "../Pages/Board.jsx";
+import BoardList from "../Pages/BoardList.jsx";
+
+const Index = () => {
+    return(
+    <>
+        <MainLayout />
+        <Board />
+        <BoardList />
+
+    </>
+    )
+}
+export default Index
