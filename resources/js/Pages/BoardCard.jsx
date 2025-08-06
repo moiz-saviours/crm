8fab6b3eea2022fe3b2b06c3e6d34bbd927e6e4a
moiz-saviours <moiz@saviours.co>
2025-08-07 04:12:56 +0500

    Hussain khan ( update ): Add BoardCard component
    Introduces a new BoardCard React component for displaying card details, including image, title, icons, and user initials. This component is styled and triggers a modal on click.

---
+++ resources/js/Pages/BoardCard.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const BoardCard = () => {
+    return (
+        <>
+            <div className="board-card" data-bs-toggle="modal" data-bs-target="#cardDetailModal"
+                 style={{cursor: "pointer"}}>
+                <div className="board-card-img">
+                    <img src="/images/card_img.webp" alt=""/>
+                </div>
+                <div className="board-content-box">
+                    <div className="priority_sec"></div>
+                    <div className="board-card-title">
+                        <h3>New Card</h3>
+                    </div>
+                    <div className="board-card-detail d-flex">
+                        <i className="fa-solid fa-eye"></i>
+                        <button className="board-card-btn">
+                            <i className="fa-solid fa-clock"></i> Jul 4
+                        </button>
+                        <i className="fa-solid fa-barcode"></i>
+                        <i className="fa-regular fa-comment"> 1</i>
+                        <div className="board-card-user">
+                            <span>M</span>
+                            <span>A</span>
+                            <span>H</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+export default BoardCard
