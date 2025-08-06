a2317760e8fa6a5e42927f620d84872da0937c45
moiz-saviours <moiz@saviours.co>
2025-08-07 04:53:02 +0500

    refactor(layout): enhance MainLayout structure
    refactor(layout): enhance MainLayout structure
    
    - Added task-layout container
    - Properly structured main content
    - Maintained header component
    - Improved JSX formatting

--- resources/js/Layouts/MainLayout.jsx
+++ resources/js/Layouts/MainLayout.jsx
@@ -1,15 +1,14 @@
 import Header from "../Components/Header.jsx";
 import React from 'react'
 
-const MainLayout = ({ children }) => {
+const MainLayout = ({children}) => {
     return (
-            <>
-                <Header/>
-                <main>
-                    {children}
-                </main>
-            </>
-
+        <div className="task-layout">
+            <Header/>
+            <main>
+                {children}
+            </main>
+        </div>
     )
 }
 export default MainLayout
