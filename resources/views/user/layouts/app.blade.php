8237319bb1fac8b49d31ee960c6e13231a584c02
moiz-saviours <moiz@saviours.co>
2025-08-08 01:44:49 +0500

    refactor(layout): improve user layout logo display
    refactor(layout): improve user layout logo display
    
    - Updated logo sizing constraints
    - Maintained responsive behavior
    - Preserved accessibility
    - Cleaner markup

--- resources/views/user/layouts/app.blade.php
+++ resources/views/user/layouts/app.blade.php
@@ -161,7 +161,7 @@
                         <img src="{{asset('assets/img/favicon.png')}}" alt="Logo" class="logo"
                              {{--                             width="16"--}}
                              {{--                             height="16"--}}
-                             style="width:110px;height:40px">
+                             style="max-width:110px;max-height:30px">
                     </a>
 
 
