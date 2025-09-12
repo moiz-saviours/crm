eeaa4df6ea27452fd81dcbeccfbe4bb44726289a
moiz-saviours <moiz@saviours.co>
2025-09-13 03:45:13 +0500

    fix(model): correct table name in EmailLabelPivot model
    fix(model): correct table name in EmailLabelPivot model
    
    - Changed table name to proper plural form 'email_label_pivots'
    - Maintained existing field definitions
    - Ensured proper database mapping

--- app/Models/EmailLabelPivot.php
+++ app/Models/EmailLabelPivot.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Pivot;
 
 class EmailLabelPivot extends Pivot
 {
-    protected $table = 'email_label_pivot';
+    protected $table = 'email_label_pivots';
 
     protected $fillable = [
         'email_id',
