8b92838d7e6991122e2e1d58f619edb8b0f48217
hasnat-stellers <hasnat.khan@stellers.org>
2025-09-27 06:26:20 +0500

    add hidden variable for reply email

--- resources/views/admin/customers/contacts/email/partials/activity.blade.php
+++ resources/views/admin/customers/contacts/email/partials/activity.blade.php
@@ -1,5 +1,5 @@
 @if (!empty($timeline) && count($timeline) > 0)
-                                    @include('admin.customers.contacts.email.static-content.email')
+    @include('admin.customers.contacts.email.static-content.email')
 
     @foreach ($timeline as $item)
         @if ($item['type'] === 'note')
@@ -46,8 +46,7 @@
                 </div>
             </div>
         @elseif ($item['type'] === 'email')
-                                    @include('admin.customers.contacts.email.static-content.email')
-
+            @include('admin.customers.contacts.email.static-content.email')
             {{-- ================= EMAIL ================= --}}
             <div class="email-box-container mb-4 border rounded bg-white p-3">
                 <div class="toggle-btnss" data-target=".content-{{ $item['data']['uuid'] }}">
@@ -69,7 +68,6 @@
                                         {{ Str::limit(collect(is_string($item['data']['to']) ? json_decode($item['data']['to'], true) ?? [$item['data']['to']] : $item['data']['to'])->map(fn($r) => is_array($r) ? $r['email'] ?? ($r['name'] ?? 'Unknown') : $r)->implode(', '),50) }}
                                     </span>
                                 </p>
-
                                 @if (
                                     !empty($item['data']['open_count']) &&
                                         $item['data']['open_count'] > 0 &&
@@ -80,9 +78,7 @@
                                         Clicks: {{ $item['data']['click_count'] ?? 0 }}
                                     </p>
                                 @endif
-
                             </div>
-
                         </div>
                         <div class="text-end" style="min-width: 160px;">
                             <p class="mb-1 text-muted small">
@@ -98,10 +94,8 @@
                         </div>
                     </div>
                 </div>
-                {{-- Collapsible details --}}
                 <div class="contentdisplaytwo {{ $item['data']['uuid'] }} mt-3 p-3 rounded border bg-light"
                     style="display: none;">
-                    {{-- Activity Timeline --}}
                     @if (
                         !empty($item['data']['open_count']) &&
                             $item['data']['open_count'] > 0 &&
@@ -145,8 +139,52 @@
                         </div>
                     @endif
 
+                    <div class="email-box-container" style="margin: 0; border-radius: 0;">
 
-                    {{-- Attachments --}}
+                        <div>
+                            <div class="new-profile-parent-wrapper">
+                                <div class="new-profile-email-wrapper">
+                                    <div class="user_profile_img">
+                                        <div class="avatarr">
+                                            {{ $item['data']['from']['name'] ? strtoupper(substr($item['data']['from']['name'], 0, 2)) : 'NA' }}
+                                        </div>
+                                    </div>
+                                    <div class="user_profile_text">
+                                        <p style="padding: 0; margin: 0;">
+                                            {{ $item['data']['from']['name'] ?? 'Unknown' }}
+                                        </p>
+                                        <p style="font-weight: 500; padding-left: 0;">
+                                            to
+                                            {{ collect(is_string($item['data']['to']) ? json_decode($item['data']['to'], true) ?? [$item['data']['to']] : $item['data']['to'])->map(fn($r) => is_array($r) ? $r['email'] ?? ($r['name'] ?? 'Unknown') : $r)->implode(', ') }}
+                                        </p>
+                                    </div>
+                                </div>
+                                <div>
+                                    <div class="new-profile-email-thread-wrapper">
+                                        <div class="activities-seprater">Reply all</div>
+                                        <div class="activities-seprater reply-btn"
+                                            data-from="{{ $item['data']['from']['email'] ?? '' }}"
+                                            data-subject="{{ $item['data']['subject'] ?? '' }}"
+                                            data-date="{{ $item['data']['date']->format('M d, Y H:i') ?? '' }}"
+                                            data-body='@json($item['data']['body']['html'] ?? $item['data']['body']['text'])'
+                                            data-thread-id="{{ $item['data']['thread_id'] ?? '' }}"
+                                            data-in-reply-to="{{ $item['data']['message_id'] ?? '' }}"
+                                            data-references='@json($item['data']['references'] ?? null)'>
+                                            Reply
+                                        </div>
+                                        <div class="activities-seprater open-email-form">Forward</div>
+                                        <div class="activities-seprater open-email-form">Delete</div>
+                                    </div>
+                                    <div>
+                                        <p>{{ $item['data']['date']->format('d/m/Y h:i A T') }}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="email-preview mb-4 text-dark small">
+                            {!! $item['data']['body']['html'] ?? nl2br($item['data']['body']['text'] ?? 'No body content available.') !!}
+                        </div>
+                    </div>
                     @if (!empty($item['data']['attachments']))
                         <div class="attachments-section mb-4">
                             <h6 class="fw-semibold text-dark mb-2">
@@ -180,228 +218,6 @@
                             </div>
                         </div>
                     @endif
-
-
-                    <div class="email-box-container" style="margin: 0; border-radius: 0;">
-                        {{-- Email Body --}}
-                        <div class="email-preview mb-4 text-dark small">
-                            {!! $item['data']['body']['html'] ?? nl2br($item['data']['body']['text'] ?? 'No body content available.') !!}
-                        </div>
-                        <div>
-                            <div class="new-profile-parent-wrapper">
-                                <div class="new-profile-email-wrapper">
-                                    <div class="user_profile_img">
-                                        <div class="avatarr">MS
-                                        </div>
-                                    </div>
-                                    <div class="user_profile_text">
-                                        <p style="padding: 0; margin: 0;">
-                                            Devod
-                                            Word </p>
-
-                                        <p style="font-weight: 500; padding-left: 0;">
-                                            to
-                                            info@hasnaat.com,
-                                            abc@gmail.com.com
-                                        </p>
-                                    </div>
-                                </div>
-
-
-                                <div>
-                                    <div class="new-profile-email-thread-wrapper">
-
-                                        <div class="activities-seprater ">
-                                            Reply all
-                                        </div>
-                                        <div class="activities-seprater ">
-                                            Reply
-                                        </div>
-
-                                        <div class="activities-seprater open-form-btn">
-                                            Forward
-                                        </div>
-                                        <div class="activities-seprater open-form-btn">
-                                            Delete
-                                        </div>
-
-
-                                    </div>
-                                    <div>
-                                        <p>12/03/2024 4:48 PM GMT+5
-                                        </p>
-                                    </div>
-
-                                </div>
-
-
-                            </div>
-
-
-                        </div>
-                        <div class="user_cont user-email-template">
-                            <p>
-                                Hi Apex Web Team, <br>
-
-                            </p><p class="email-body-text">
-                                I hope you're doing well. I
-                                specialize
-                                in
-                                online
-                                reputation management and can help
-                                boost
-                                your
-                                business's presence by generating
-                                positive
-                                reviews
-                                and addressing any negative
-                                feedback.
-
-                            </p>
-                            <div class="moretext">
-                                <p class="email-body-text">
-                                    I hope you're doing well. I
-                                    specialize
-                                    in online
-                                    reputation management and can
-                                    help
-                                    boost
-                                    your
-                                    business's presence by
-                                    generating
-                                    positive reviews
-                                    and addressing any negative
-                                    feedback. I
-                                    specialize
-                                    in online
-                                    reputation management and can
-                                    help
-                                    boost
-                                    your
-                                    business.
-
-                                </p>
-                                <br>
-                                <p>--</p>
-
-                                <p class="email-body-text">
-                                    Street Hospitality <br>
-                                    <span class="activities-seprater">
-                                                                                                                                        +1-97-676552
-                                                                                                                                    </span>
-                                    <br>
-                                    <a href="#" class="email-client-site-link">
-                                        Visit our website!</a>
-                                </p>
-
-                                <img src="http://localhost:8000/assets/images/logo-img.png" class="img-fluid client-email-logo">
-
-                                <br>
-                                <br>
-                            </div>
-
-
-                            <div class="doc-attachment-container">
-                                <a href="path/to/your/file.pdf" class="doc-attachment">
-                                    <div class="icon-doc">
-                                        📎
-                                    </div>
-                                    <div class="file-info-doc">
-                                        <p class="file-name-doc">
-                                            Document.pdf
-                                        </p>
-                                        <p class="file-size-doc">2
-                                            MB
-                                        </p>
-                                    </div>
-                                </a>
-                            </div>
-
-                            <div class="comment-head-thread-wrapper">
-
-
-                                <div>
-
-                                    <div class="email-child-wrapper">
-                                        <!-- <a
-                                    class="moreless-button activities-addition-links">
-                                    Read more</a> -->
-                                        <a class="moreless-button activities-addition-links">See
-                                            more</a>
-                                    </div>
-                                </div>
-                                <div>
-
-                                    <div class="thread-dropdown-display">
-                                        <button type="button" class="dropdown-toggle custom-drop-btn-design" id="dropdownMenuButtonthread" data-bs-toggle="dropdown" aria-expanded="false">
-                                            3
-                                            association
-                                        </button>
-                                        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButtonthread">
-                                            <div>
-
-                                                <div class="dropdown-content-wraper">
-                                                    <ul class="nested-select-list">
-                                                        <li class="checkbox-item">
-
-                                                            <label>Companies
-                                                                0</label>
-                                                        </li>
-                                                        <li class="checkbox-item">
-
-                                                            <label>Carts
-                                                                0</label>
-                                                        </li>
-                                                        <li class="checkbox-item">
-
-                                                            <label>Contacts
-                                                                0</label>
-                                                        </li>
-                                                        <li class="checkbox-item">
-
-                                                            <label>Leads
-                                                                0</label>
-                                                        </li>
-                                                        <li class="checkbox-item">
-
-                                                            <label>Deals
-                                                                0</label>
-                                                        </li>
-                                                        <li class="checkbox-item">
-
-                                                            <label>Orders
-                                                                0</label>
-                                                        </li>
-                                                        <!-- Add more items as needed -->
-                                                    </ul>
-                                                    <div>
-                                                        <div class="search-box-select">
-                                                            <input type="text" placeholder="Search current associations" class="search-input">
-                                                        </div>
-                                                        <div class="select-contact-box-space">
-                                                            <p class="select-contact">
-                                                                Contacts
-                                                            </p>
-
-                                                            <input type="checkbox" id="contact2">
-                                                            <label for="contact2">HoeoSQMLp
-                                                                becelhmerthewatt@yahoo.com</label>
-                                                        </div>
-                                                    </div>
-
-                                                </div>
-                                            </div>
-                                        </ul>
-                                    </div>
-                                </div>
-
-
-                            </div>
-
-
-                        </div>
-
-                    </div>
                 </div>
             </div>
         @endif
