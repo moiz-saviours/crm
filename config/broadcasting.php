bf25042e6f5c78cb790dae3cb98542a79d8d19d2
moiz-saviours <moiz@saviours.co>
2025-10-21 03:01:28 +0500

    Update socketio broadcasting config options
    Changed the driver name from 'socket.io' to 'socketio', added 'url' and 'options' keys to the socketio connection, and set 'secure' based on the environment. This improves configuration flexibility and environment-specific security.

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -3,10 +3,14 @@ return [
 
     'connections' => [
         'socketio' => [
-            'driver' => 'socket.io',
+            'driver' => 'socketio',
             'host' => env('SOCKETIO_HOST', 'localhost'),
             'port' => env('SOCKETIO_PORT', 6001),
+            'url' => env('SOCKETIO_URL', env('APP_URL')),
             'namespace' => '\App\Broadcasting',
+            'options' => [
+                'secure' => env('APP_ENV') === 'production',
+            ],
         ],
     ],
 ];
