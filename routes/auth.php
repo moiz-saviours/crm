3109462755fc294e56ec1bbf9c5cdbdb6d847708
moiz-saviours <moiz@saviours.co>
2025-10-29 23:02:27 +0500

    Update auth.php

--- routes/auth.php
+++ routes/auth.php
@@ -20,14 +20,14 @@ Route::middleware(['guest:web'])->group(function () {
     Route::get('login', [AuthenticatedSessionController::class, 'create'])
         ->name('login');
     Route::post('login', [AuthenticatedSessionController::class, 'store']);
-    Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
-        ->name('password.request');
-    Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])
-        ->name('password.email');
-    Route::get('reset-password/{token}', [NewPasswordController::class, 'create'])
-        ->name('password.reset');
-    Route::post('reset-password', [NewPasswordController::class, 'store'])
-        ->name('password.store');
+//    Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
+//        ->name('password.request');
+//    Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])
+//        ->name('password.email');
+//    Route::get('reset-password/{token}', [NewPasswordController::class, 'create'])
+//        ->name('password.reset');
+//    Route::post('reset-password', [NewPasswordController::class, 'store'])
+//        ->name('password.store');
 
 });
 Route::middleware('auth:web')->group(function () {
