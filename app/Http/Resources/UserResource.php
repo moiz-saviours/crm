8ebfcda9ed29fdb82b212a693160e73acac88e0b
moiz-saviours <moiz@saviours.co>
2025-10-30 21:40:52 +0500

    Revert "feat(customer-contact): add API token support and improve code formatting"
    This reverts commit 43648832033343b566fb7259892a2086353916c5.

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -18,13 +18,7 @@ class UserResource extends JsonResource
             'id' => $this->id,
             'name' => $this->name,
             'email' => $this->email,
-            'image' => $this->image ? asset('assets/images/employees/' . $this->image) : null,
-            "phone" => "1234567890",
-            "ip_address" => null,
-            "status" => 1,
-            "deleted_at" => null,
-            "updated_at" => "2025-02-06T17:42:05.000000Z",
-            "created_at" => "2025-02-04T18:50:32.000000Z"
+            'image' => asset('assets/images/employees/'.$this->image),
         ];
     }
 }
