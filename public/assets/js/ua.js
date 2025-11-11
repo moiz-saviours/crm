bf2152d0400a75c0b708c36cff429b7f25f70dbc
moiz-saviours <moiz@saviours.co>
2025-11-11 06:03:21 +0500

    Update ua.js

--- public/assets/js/ua.js
+++ public/assets/js/ua.js
@@ -1,26 +1,36 @@
-var url = new URL(document.currentScript.src);
-const token = url.searchParams.get("token");
-!function (t, e, r) {
-    if (!document.getElementById(t)) {
-        var basePath = document.currentScript.src.substring(0, currentScriptSrc.lastIndexOf('/'));
-        var n = document.createElement("script");
-        for (var a in n.src = basePath + "/user-activity.js?token="+token, n.type = "text/javascript", n.id = t, r) r.hasOwnProperty(a) && n.setAttribute(a, r[a]);
-        var i = document.getElementsByTagName("script")[0];
-        i.parentNode.insertBefore(n, i)
-    }
-}("CollectedForms", 0, {
-    "crossorigin": "anonymous",
-    "data-token": token,
-});
-!function (t, e, r) {
-    if (!document.getElementById(t)) {
-        var basePath = document.currentScript.src.substring(0, currentScriptSrc.lastIndexOf('/'));
-        var n = document.createElement("script");
-        for (var a in n.src = basePath + "/wl-script.js?token="+token, n.type = "text/javascript", n.id = t, r) r.hasOwnProperty(a) && n.setAttribute(a, r[a]);
-        var i = document.getElementsByTagName("script")[0];
-        i.parentNode.insertBefore(n, i)
-    }
-}("CollectedForms", 0, {
-    "crossorigin": "anonymous",
-    "data-token": token,
-});
+(() => {
+    var url = new URL(document.currentScript.src);
+    const token = url.searchParams.get("token");
+
+    (function (t, e, r) {
+        if (!document.getElementById(t)) {
+            var basePath = document.currentScript.src.substring(0, document.currentScript.src.lastIndexOf('/'));
+            var n = document.createElement("script");
+            for (var a in r) if (r.hasOwnProperty(a)) n.setAttribute(a, r[a]);
+            n.src = basePath + "/user-activity.js?token=" + token;
+            n.type = "text/javascript";
+            n.id = t;
+            var i = document.getElementsByTagName("script")[0];
+            i.parentNode.insertBefore(n, i);
+        }
+    })("CollectedForms1", 0, {
+        "crossorigin": "anonymous",
+        "data-token": token,
+    });
+
+    (function (t, e, r) {
+        if (!document.getElementById(t)) {
+            var basePath = document.currentScript.src.substring(0, document.currentScript.src.lastIndexOf('/'));
+            var n = document.createElement("script");
+            for (var a in r) if (r.hasOwnProperty(a)) n.setAttribute(a, r[a]);
+            n.src = basePath + "/wl-script.js?token=" + token;
+            n.type = "text/javascript";
+            n.id = t;
+            var i = document.getElementsByTagName("script")[0];
+            i.parentNode.insertBefore(n, i);
+        }
+    })("CollectedForms2", 0, {
+        "crossorigin": "anonymous",
+        "data-token": token,
+    });
+})();
